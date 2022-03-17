2f558fdcf55bf82d91d023a8d88a88c7b9c33912
DHARMIK  GANGANI <dharmikgangani786@gmail.com>
2022-03-17 10:45:40 +0530

    Update client.go

--- client.go
+++ client.go
@@ -1,68 +1,70 @@
 package main
 
-import "net"
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"strings"
+)
 
-type client struct{
-	conn net.Addr
-	nick string
-	room *room
+type client struct {
+	conn     net.Conn
+	nick     string
+	room     *room
 	commands chan<- command
 }
 
-func(c *client) readInput(){
+func (c *client) readInput() {
 	for {
-		msg, err := bufio.NewReader(c.conn)ReadString('\n')
-		if err != nil{
+		msg, err := bufio.NewReader(c.conn).ReadString('\n')
+		if err != nil {
 			return
 		}
+
 		msg = strings.Trim(msg, "\r\n")
 
 		args := strings.Split(msg, " ")
 		cmd := strings.TrimSpace(args[0])
 
-		switch cmd{
+		switch cmd {
 		case "/nick":
 			c.commands <- command{
-				id: CMD_NICK,
+				id:     CMD_NICK,
 				client: c,
-				args: args,
+				args:   args,
 			}
 		case "/join":
 			c.commands <- command{
-				id: CMD_JOIN,
+				id:     CMD_JOIN,
 				client: c,
-				args: args,
+				args:   args,
 			}
 		case "/rooms":
 			c.commands <- command{
-				id: CMD_ROOMS,
+				id:     CMD_ROOMS,
 				client: c,
-				args: args,
 			}
 		case "/msg":
 			c.commands <- command{
-				id: CMD_MSG,
+				id:     CMD_MSG,
 				client: c,
-				args: args,
+				args:   args,
 			}
 		case "/quit":
 			c.commands <- command{
-				id: CMD_QUIT,
+				id:     CMD_QUIT,
 				client: c,
-				args: args,
 			}
 		default:
-			c.err(fmt.Errorf("unknown command: %s",cmd))
-
+			c.err(fmt.Errorf("unknown command: %s", cmd))
 		}
 	}
 }
 
-func(c *client) err(err error){
-	c.conn.Write([]byte("ERR:" + err.Error() + "\n"))
+func (c *client) err(err error) {
+	c.conn.Write([]byte("err: " + err.Error() + "\n"))
 }
 
-func(c *client) msg(msg string){
-	c.conn.Write([]byte(">" + msg + "\n"))
+func (c *client) msg(msg string) {
+	c.conn.Write([]byte("> " + msg + "\n"))
 }
-
