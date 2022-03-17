06aeeb4131044ed0ef7de24ced22420a1d363b11
DHARMIK  GANGANI <dharmikgangani786@gmail.com>
2022-03-17 10:20:17 +0530

    Update client.go

--- client.go
+++ client.go
@@ -1,29 +1,68 @@
 package main
 
-import (
-	"log"
-	"net"
-)
+import "net"
 
-func main() {
-	s := newsever()
-	go s.run()
+type client struct{
+	conn net.ddr
+	nick string
+	room *room
+	commands chan<- command
+}
 
-	listener,err := net.Listne("tcp",":8888")
-	if err !=nil{
-		log.Fatalf("unable to start server: %s",err.Error())
-	}
-
-	defer listener.Close()
-	log.Printf("sterted server on :8888")
-
-	for{
-		conn,err := listener.Accept()
+func(c *client) readInput(){
+	for {
+		msg, err := bufio.NewReader(c.conn)ReadString('\n')
 		if err != nil{
-			log/Print("unable to accept connection: %s",err.Error())
-			continue
+			return
 		}
+		msg = strings.Trim(msg, "\r\n")
 
-		go s.newClient(conn)
+		args := strings.Split(msg, " ")
+		cmd := strings.TrimSpace(args[0])
+
+		switch cmd{
+		case "/nick":
+			c.commands <- command{
+				id: CMD_NICK,
+				client: c,
+				args: args,
+			}
+		case "/join":
+			c.commands <- command{
+				id: CMD_JOIN,
+				client: c,
+				args: args,
+			}
+		case "/rooms":
+			c.commands <- command{
+				id: CMD_ROOMS,
+				client: c,
+				args: args,
+			}
+		case "/msg":
+			c.commands <- command{
+				id: CMD_MSG,
+				client: c,
+				args: args,
+			}
+		case "/quit":
+			c.commands <- command{
+				id: CMD_QUIT,
+				client: c,
+				args: args,
+			}
+		default:
+			c.err(fmt.Errorf("unknown command: %s",cmd))
+
+		}
 	}
 }
+
+func(c *client) err(err error){
+	c.conn.Write([]byte("ERR:" + err.Error() + "\n"))
+}
+
+func(c *client) msg(msg string){
+	c.conn.Write([]byte(">" + msg + "\n"))
+}
+
