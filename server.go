47f69ec67c47fec2000c9aec30f3a1fdfabb91b5
DHARMIK  GANGANI <dharmikgangani786@gmail.com>
2022-03-17 10:45:13 +0530

    Update server.go

--- server.go
+++ server.go
@@ -1,95 +1,119 @@
 package main
 
-type server struct{
-	rooms map[string]*room
+import (
+	"fmt"
+	"log"
+	"net"
+	"strings"
+)
+
+type server struct {
+	rooms    map[string]*room
 	commands chan command
 }
 
-func newServer() *server{
+func newServer() *server {
 	return &server{
-		rooms: make(map[string]*room),
-		commands:make(chan command),
+		rooms:    make(map[string]*room),
+		commands: make(chan command),
 	}
 }
-func(s *server) run(){
-	for cmd := range s.commands{
+
+func (s *server) run() {
+	for cmd := range s.commands {
 		switch cmd.id {
 		case CMD_NICK:
 			s.nick(cmd.client, cmd.args)
 		case CMD_JOIN:
-			s.join(cmd.client,cmd.args)
+			s.join(cmd.client, cmd.args)
 		case CMD_ROOMS:
-			s.listRooms(cmd.client,cmd.args)
+			s.listRooms(cmd.client)
 		case CMD_MSG:
-			s.msg(cmd.client,cmd.args)
+			s.msg(cmd.client, cmd.args)
 		case CMD_QUIT:
-			s.quit(cmd.client,cmd.args)
+			s.quit(cmd.client)
 		}
 	}
 }
-func(s*server) newClient(conn net.Conn){
-	log.Print("new client has connected:%s",conn.ddr().String())
 
-	c :=&client{
-		conn:conn,
-		nick:"anonymous",
-		commands: s.commands
+func (s *server) newClient(conn net.Conn) *client {
+	log.Printf("new client has joined: %s", conn.RemoteAddr().String())
+
+	return &client{
+		conn:     conn,
+		nick:     "anonymous",
+		commands: s.commands,
 	}
-	c.readInput()
 }
 
-func(s *server) nick(c *client, args []string){
-	c.nick =args[1]
-	c.msg(fmt.Sprintf("all right,I will call you %s", c.nicks))
+func (s *server) nick(c *client, args []string) {
+	if len(args) < 2 {
+		c.msg("nick is required. usage: /nick NAME")
+		return
+	}
+
+	c.nick = args[1]
+	c.msg(fmt.Sprintf("all right, I will call you %s", c.nick))
 }
-func(s *server) join(c *client, args []string){
+
+func (s *server) join(c *client, args []string) {
+	if len(args) < 2 {
+		c.msg("room name is required. usage: /join ROOM_NAME")
+		return
+	}
+
 	roomName := args[1]
-	r,ok := s.rooms[roomName]
+
+	r, ok := s.rooms[roomName]
 	if !ok {
 		r = &room{
-			name:roomName,
+			name:    roomName,
 			members: make(map[net.Addr]*client),
 		}
-		s.room[roomName] = r
+		s.rooms[roomName] = r
 	}
 	r.members[c.conn.RemoteAddr()] = c
 
-	s.quitCurrentRoom(call)
+	s.quitCurrentRoom(c)
+	c.room = r
 
-	c.room=r
-	 r.broadcaste(c, fmt.Sprintf("%s has joined the room",c.nick))
-	 c.msg(fmt.Sprintf("welcome to %s",r.name))
+	r.broadcast(c, fmt.Sprintf("%s joined the room", c.nick))
+
+	c.msg(fmt.Sprintf("welcome to %s", roomName))
 }
 
-func(s *server) listRooms(c *client, args []string){
+func (s *server) listRooms(c *client) {
 	var rooms []string
-	for name:= range s.rooms{
-		rooms = append(rooms,name)
-
+	for name := range s.rooms {
+		rooms = append(rooms, name)
 	}
-	c.msg(fmt.Sprintf("available rooms are:%s",string.Join(rooms,",")))
+
+	c.msg(fmt.Sprintf("available rooms: %s", strings.Join(rooms, ", ")))
 }
 
-func(s *server) msg(c *client, args []string){
-	if c.room == nil{
-		c.err(errors.New("you must join the room first"))
+func (s *server) msg(c *client, args []string) {
+	if len(args) < 2 {
+		c.msg("message is required, usage: /msg MSG")
 		return
 	}
-	c.room.broadcaste(c,c.nick+":"strings.join(args[1:len(args)],""))
+
+	msg := strings.Join(args[1:], " ")
+	c.room.broadcast(c, c.nick+": "+msg)
 }
 
-func(s *server) quit(c *client, args []string){
-	 log.Print("client has disconnected :%s",c.conn.RemoteAddr().String())
+func (s *server) quit(c *client) {
+	log.Printf("client has left the chat: %s", c.conn.RemoteAddr().String())
 
-	 s.quitCurrentRoom(c)
+	s.quitCurrentRoom(c)
 
-	 c.msg("sad to see you go :(")
-	 c.conn.Close
+	c.msg("sad to see you go =(")
+	c.conn.Close()
 }
 
-func (s *server) quitCurrentRoom(c *client){
-	if c.room != nil{
-		delete(c.room.members,c.conn, RemoteAddr())
-		c.room.broadcaste(c, fmt.Sprintf("%s has left the room",c.nick))
+func (s *server) quitCurrentRoom(c *client) {
+	if c.room != nil {
+		oldRoom := s.rooms[c.room.name]
+		delete(s.rooms[c.room.name].members, c.conn.RemoteAddr())
+		oldRoom.broadcast(c, fmt.Sprintf("%s has left the room", c.nick))
 	}
 }
