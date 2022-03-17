15db2beae19444ffc002c0fe315b1c76796de5e1
DHARMIK  GANGANI <dharmikgangani786@gmail.com>
2022-03-17 10:46:06 +0530

    Update command.go

--- command.go
+++ command.go
@@ -2,15 +2,16 @@ package main
 
 type commandID int
 
-const{
-	CMD_NICK commandID=iota
+const (
+	CMD_NICK commandID = iota
 	CMD_JOIN
 	CMD_ROOMS
 	CMD_MSG
 	CMD_QUIT
-}
-type command struct{
-	id commandID
+)
+
+type command struct {
+	id     commandID
 	client *client
-	args []string
+	args   []string
 }
