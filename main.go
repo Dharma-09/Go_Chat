62941691430ada59252d31a1e1090230550fc82a
DHARMIK  GANGANI <dharmikgangani786@gmail.com>
2022-03-17 10:46:51 +0530

    Update main.go

--- main.go
+++ main.go
@@ -6,24 +6,25 @@ import (
 )
 
 func main() {
-	s := newsever()
+	s := newServer()
 	go s.run()
 
-	listener,err := net.Listne("tcp",":8888")
-	if err !=nil{
-		log.Fatalf("unable to start server: %s",err.Error())
+	listener, err := net.Listen("tcp", ":8888")
+	if err != nil {
+		log.Fatalf("unable to start server: %s", err.Error())
 	}
 
 	defer listener.Close()
-	log.Printf("sterted server on :8888")
+	log.Printf("server started on :8888")
 
-	for{
-		conn,err := listener.Accept()
-		if err != nil{
-			log/Print("unable to accept connection: %s",err.Error())
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("failed to accept connection: %s", err.Error())
 			continue
 		}
 
-		go s.newClient(conn)
+		c := s.newClient(conn)
+		go c.readInput()
 	}
 }
